017f39fe742e2f8c354fabc614543489b74fa6cf
renlu <905937702@qq.com>
2019-02-19 18:56:19 +0800

    services

--- app/service/news.js
+++ app/service/news.js
@@ -1,1 +1,17 @@
 const Service = require('egg').Service
+
+class newsService extends Service{
+  async getNews(){
+
+    const dataList = {
+      list : [
+          {name:'章三',age:12},
+          {name:'里斯',age:34}
+        ]
+      }
+
+    return dataList
+  }
+}
+
+module.exports = newsService
